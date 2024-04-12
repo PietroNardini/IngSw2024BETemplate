d6b9b1eed9b6606a8cfd187301844bf44e0fa373
alazzarini <lzzlsn@unife.it>
2024-04-12 15:36:07 +0200

    REST

--- src/main/java/it/unife/ingsw202324/MicroservizioBase/api/MainController.java
+++ src/main/java/it/unife/ingsw202324/MicroservizioBase/api/MainController.java
@@ -2,6 +2,7 @@ package it.unife.ingsw202324.MicroservizioBase.api;
 
 import it.unife.ingsw202324.MicroservizioBase.models.MyTable;
 import it.unife.ingsw202324.MicroservizioBase.services.MyService;
+import it.unife.ingsw202324.MicroservizioBase.services.TemplateRestConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,10 @@ public class MainController {
 
         /* Chiamata a un servizio che ritorna inserisce 3 dati e ritorna il db */
         return myService.addElements();
+    }
+
+    @RequestMapping("/callREST") /* Annotation per definire il path del metodo (relativo alla classe)  */
+    public String callRest() {
+        return TemplateRestConsumer.callREST("helloMock", null, true);
     }
 }
